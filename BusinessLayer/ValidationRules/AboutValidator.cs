a3b8685cce5309724795a88c8136039c3f75c22f
aktepeayse96 <aktepeayse96@gmail.com>
2023-06-05 23:30:59 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklma kısmı boş geçilemez");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("lütfen görsel seçiniz");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("lütfen en az 50 karakterli görsel seçiniz");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("lütfen açıklamayı kısaltın");
+
+        }
+    }
+}
---
+++ TraversalCoreProje/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            //burada view le featuredb den değerleri viewbagdan getirecekti sonra soru sorup kodlamadı
+            return View(values);
+        }
+    }
+}
